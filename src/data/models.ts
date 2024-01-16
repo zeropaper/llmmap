0f24b8403766282418a0ad3f18c97e08f1e10e35
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2024-01-16 10:29:05 +0100

    work on pages

---
+++ src/data/models.ts
@@ -0,0 +1,17 @@
+export const models = [
+  {
+    modelName: 'mistral-tiny',
+  },
+  {
+    modelName: 'mistral-small',
+  },
+  {
+    modelName: 'mistral-medium',
+  },
+  {
+    modelName: 'gpt-3.5-turbo-1106',
+  },
+  {
+    modelName: 'gpt-4-0613',
+  }
+];
